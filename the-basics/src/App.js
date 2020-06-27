e520a2c249e9d966c55ac39ba4f5aca2483ca295
Uriel Soubran <uriel_bee15@hotmail.com>
2020-06-27 09:42:16 -0500

    Basics
    Acá estan los archivos creados con create-react-app en el módulo de Basics

--- the-basics/src/App.js
+++ the-basics/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    /* return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hola, esto es un H1 desde React</h1>
       </div>
-    );
+    ); */
+
+    return React.createElement("div", {className: 'App'}, React.createElement("h1", null, "Forma larga de componente :c"));
+
   }
 }
 
