2c4787a92724d4135c0e40f9ba7d98143c7ca92f
Uriel Soubran <uriel_bee15@hotmail.com>
2020-06-30 03:50:01 -0500

    Hasta Manipulating state

--- the-basics/src/App.js
+++ the-basics/src/App.js
@@ -3,14 +3,37 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    personas: [
+      {name: 'Uriel', age: 24},
+      {name: 'Claudio', age: 27},
+      {name: 'Wendy', age: 22}
+    ]
+  };
+
+
+  switchNameHandler = () => {
+    //console.log('Presionaste el botón!');
+    // NO ESCRIBAS this.state.personas[0].name = 'Alejandro'
+    this.setState({
+      personas: [
+        {name: 'Alejandro', age: 24},
+        {name: 'Claudio', age: 27},
+        {name: 'Wendy', age: 18}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <p>App component</p>
-        <Person name="Uriel" age="24"/>
-        <Person name="Claudio" age="27"> Ingeniería Industrial </Person>
-        <Person name="Wendy" age="22"/>
+        <button onClick = {this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.personas[0].name} age={this.state.personas[0].age}/>
+        <Person name={this.state.personas[1].name} age={this.state.personas[1].age}> Ingeniería Industrial </Person>
+        <Person name={this.state.personas[2].name} age={this.state.personas[2].age}/>
       </div>
     );
   }
